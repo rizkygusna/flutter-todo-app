d0ad045e9e1a680f2d8bc14086c1ca9cbecfc9c0
rizkygusna <gusnafarid@gmail.com>
2022-03-15 23:48:46 +0700

    add data stream with firestore instance

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 // import 'package:flutter/rendering.dart';
 
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:todo_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:todo_app/firebase_options.dart';
 
-// Future<void> main() async {
-//   // Initialize Firebase
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//   runApp(const MyApp());
-// }
-
-void main() {
+Future<void> main() async {
+  // Initialize Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
+}
+
+class Todo {
+  String todo = '';
+  bool isChecked = false;
+
+  Todo(String todo, bool isChecked) {
+    this.todo;
+    this.isChecked;
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -64,33 +72,56 @@ class _TodoListState extends State<TodoList> {
     {'todo': 'Buy bread', 'isChecked': 'false'},
   ];
 
+  final Stream<QuerySnapshot> _todosStream =
+      FirebaseFirestore.instance.collection('todos').snapshots();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: ListView(
-            padding: const EdgeInsets.all(8),
-            children: _todos
-                .map((item) => Container(
-                      margin: const EdgeInsets.symmetric(vertical: 4),
-                      decoration: _boxStyle,
-                      child: CheckboxListTile(
-                          dense: true,
-                          contentPadding:
-                              const EdgeInsets.symmetric(horizontal: 4),
-                          controlAffinity: ListTileControlAffinity.leading,
-                          // make sure item['todo'] is not null by using !
-                          title: Text(item['todo']!,
-                              style: const TextStyle(fontSize: 16)),
-                          value: item['isChecked'] == 'true' ? true : false,
-                          onChanged: (bool? val) {
-                            setState(() {
-                              item['isChecked'] = val.toString();
-                            });
-                          }),
-                    ))
-                .toList()));
+    return StreamBuilder<QuerySnapshot>(
+        stream: _todosStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('Loading..');
+          }
+          return Scaffold(
+            appBar: AppBar(title: Text(widget.title)),
+            body: ListView(
+                children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> data =
+                  document.data() as Map<String, dynamic>;
+              return Text(data['todo']);
+            }).toList()),
+          );
+        });
+
+    // return Scaffold(
+    //     appBar: AppBar(
+    //       title: Text(widget.title),
+    //     ),
+    //     body: ListView(
+    //         padding: const EdgeInsets.all(8),
+    //         children: _todos
+    //             .map((item) => Container(
+    //                   margin: const EdgeInsets.symmetric(vertical: 4),
+    //                   decoration: _boxStyle,
+    //                   child: CheckboxListTile(
+    //                       dense: true,
+    //                       contentPadding:
+    //                           const EdgeInsets.symmetric(horizontal: 4),
+    //                       controlAffinity: ListTileControlAffinity.leading,
+    //                       // make sure item['todo'] is not null by using !
+    //                       title: Text(item['todo']!,
+    //                           style: const TextStyle(fontSize: 16)),
+    //                       value: item['isChecked'] == 'true' ? true : false,
+    //                       onChanged: (bool? val) {
+    //                         setState(() {
+    //                           item['isChecked'] = val.toString();
+    //                         });
+    //                       }),
+    //                 ))
+    //             .toList()));
   }
 }
